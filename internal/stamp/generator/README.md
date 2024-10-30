a9267582e58074604178276950bccd4fc27fa321
Skip Baney <twelvelabs@gmail.com>
2024-10-29 23:59:25 -0500

    feat: add default generator

---
+++ internal/stamp/generator/README.md
@@ -0,0 +1,5 @@
+# Default Generator
+
+This is the default generator that comes pre-installed with stamp. It generates new generators.
+
+![yo dog](https://i.imgflip.com/98dgrp.jpg)
--- internal/stamp/store_test.go
+++ internal/stamp/store_test.go
@@ -1,10 +1,12 @@
 package stamp
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/twelvelabs/termite/testutil"
 )
 
 // NewTestStore returns a new store pointing to ./testdata/generators.
@@ -13,6 +15,38 @@ import (
 func NewTestStore() *Store {
 	storePath, _ := filepath.Abs(filepath.Join("testdata", "generators"))
 	return NewStore(storePath)
+}
+
+func TestStore_Init(t *testing.T) {
+	testutil.InTempDir(t, func(tmpDir string) {
+		store := NewStore(tmpDir)
+
+		genDir := filepath.Join(tmpDir, "generator")
+		assert.NoDirExists(t, genDir)
+
+		err := store.Init()
+		assert.NoError(t, err)
+		assert.DirExists(t, genDir)
+		assert.DirExists(t, filepath.Join(genDir, "_src"))
+		assert.FileExists(t, filepath.Join(genDir, "generator.yaml"))
+	})
+}
+
+func TestStore_Init_WhenAlreadyExists(t *testing.T) {
+	testutil.InTempDir(t, func(tmpDir string) {
+		store := NewStore(tmpDir)
+
+		genDir := filepath.Join(tmpDir, "generator")
+		_ = os.Mkdir(genDir, 0777)
+		_ = os.WriteFile(filepath.Join(genDir, "hello.txt"), []byte("hi"), 0600)
+
+		err := store.Init()
+		assert.NoError(t, err)
+		assert.DirExists(t, genDir)
+		assert.FileExists(t, filepath.Join(genDir, "hello.txt"))
+		assert.NoDirExists(t, filepath.Join(genDir, "_src"))
+		assert.NoFileExists(t, filepath.Join(genDir, "generator.yaml"))
+	})
 }
 
 func TestStore_LoadAll(t *testing.T) {
