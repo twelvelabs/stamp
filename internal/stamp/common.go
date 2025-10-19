c429428873c3ab2ba31932eaee88ffc68f3eed8d
Skip Baney <twelvelabs@gmail.com>
2025-10-19 16:27:53 -0500

    fix: restore missing docs for common task properties

--- internal/stamp/common.go
+++ internal/stamp/common.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/spf13/cast"
-	"github.com/swaggest/jsonschema-go"
 	"github.com/twelvelabs/termite/render"
 )
 
@@ -14,38 +13,8 @@ var (
 )
 
 type Common struct {
-	IfTpl   render.Template `mapstructure:"if" default:"true"`
-	EachTpl render.Template `mapstructure:"each"`
-}
-
-// PrepareJSONSchema implements the jsonschema.Preparer interface.
-func (c Common) PrepareJSONSchema(schema *jsonschema.Schema) error {
-	if prop, ok := schema.Properties["if"]; ok {
-		prop.TypeObjectEns().
-			WithTitle("If").
-			WithDescription(
-				"Determines whether the task should be executed. "+
-					"The value must be [coercible](https://pkg.go.dev/strconv#ParseBool) "+
-					"to a boolean.",
-			).
-			WithExamples(
-				"true",
-				"{{ .SomeBool }}",
-			)
-	}
-	if prop, ok := schema.Properties["each"]; ok {
-		prop.TypeObjectEns().
-			WithTitle("Each").
-			WithDescription(
-				"Set to a comma separated value and the task will be executued once per-item. "+
-					"On each iteration, the `_Item` and `_Index` values will be set accordingly.",
-			).
-			WithExamples(
-				"foo, bar, baz",
-				"{{ .SomeList | join \",\" }}",
-			)
-	}
-	return nil
+	IfTpl   render.Template `mapstructure:"if" title:"If" default:"true" examples:"[\"true\", \"{{ .SomeBool }}\"]" description:"Determines whether the task should be executed. The value must be [coercible](https://pkg.go.dev/strconv#ParseBool) to a boolean."`                                   //nolint:lll
+	EachTpl render.Template `mapstructure:"each" title:"Each" examples:"[\"foo, bar, baz\", \"{{ .SomeList | join \\\",\\\" }}\"]" description:"Set to a comma separated value and the task will be executued once per-item. On each iteration, the _Item and _Index values will be set accordingly."` //nolint:lll
 }
 
 func (c *Common) Iterator(values map[string]any) []any {
